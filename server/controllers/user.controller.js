c262b6323204f26f114adcb0ab924e355a0faf0c
RyanCWithers <rcwithers17@gmail.com>
2021-01-19 11:59:16 -0500

    Updated login and logout controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,15 +1,21 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     registerUser: (req, res) =>{
         User.create(req.body)
             .save()
-            .then(user => res.json(user))
-            .catch(error => res.json(error));
+            .then(user => {
+                const userToken = jwt.sign({id: user._id}, process.env.SECRET_KEY);
+                res
+                    .cookie('usertoken', userToken, process.env.SECRET_KEY, {httpOnly: true})
+                    .json(user)
+            })
+            .catch(err => res.json(err));
     },
 
-    login: async(req, res) =>{
+    loginUser: async(req, res) =>{
         const user = await User.findOne({email: req.body.email});
 
         if(user === null){
@@ -17,5 +23,22 @@ module.exports = {
         }
 
         const correctPassword = await bcrypt.compare(req.body.password, user.password);
+        
+        if(!correctPassword) {
+            return(res.json('Invalid login attempt!'))
+        }
+
+        const userToken = jwt.sign({id: user._id}, process.env.SECRET_KEY);
+
+        res
+            .cookie("usertoken", userToken, process.env.SECRET_KEY, {httpOnly: true})
+            .json('Successful login attempt!');
+    },
+
+    logoutUser: (req, res) =>{
+        res.clearCookie('usertoken');
+        res.json('Succesful logout');
     }
+    
+
 }
