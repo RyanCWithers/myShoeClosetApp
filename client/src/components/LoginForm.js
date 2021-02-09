15d9f7dde35825dc3101b97c72e21a70687ee185
RyanCWithers <rcwithers17@gmail.com>
2021-02-09 10:58:02 -0500

    Updated login form with bootstrap and validators

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,12 +6,14 @@ const LoginForm = (props) =>{
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const [fieldEmpty, setFieldEmpty] = useState({emailMsg:'', passwordMsg: ''});
     const [errs, setErrs] = useState('');
 
     const onSubmitHandler = (e) =>{
         e.preventDefault();
-        axios.post("http://localhost:8000/api/myShoeCloset/login/", 
+        if(email !== '' && password !== ''){
+            setFieldEmpty({});
+            axios.post("http://localhost:8000/api/myShoeCloset/login/", 
             {email, password},
             {withCredentials: true}
             )
@@ -25,52 +27,83 @@ const LoginForm = (props) =>{
                 }
             })
             .catch(err => console.log(err));
+        } else {
+            setErrs('');
+            if(email === '' && password === ''){
+                setFieldEmpty({emailMsg: 'Please enter your email!', passwordMsg: 'Please enter your password!'});
+            } else if(email === ''){
+                setFieldEmpty({emailMsg: 'Please enter your email!', passwordMsg: ''});
+            } else {
+                setFieldEmpty({emailMsg: '', passwordMsg: 'Please enter your password!'});
+            }
+        }
+        
     }
 
     return(
-        <div className = "container">
-            <div className = "card p-4">
-                <h3 className = "card-title">User Login</h3> 
-                <form onSubmit = {onSubmitHandler} className = "card-body">
-                    <div className = "form-group row justify-content-center mb-2">
-                        <label htmlFor = "email" className = "col-form-label col-md-4">Email</label>
-                        <div className = "col-md-8">
-                            <input 
-                                type = "text"
-                                name = "email"
-                                onChange = {(e) => setEmail(e.target.value)}
-                                placeholder = "Please enter your email."
-                                className = "form-control"
-                            />
+        <div className = "container-sm">
+            <div className = "row">
+                <div className = "card p-4">
+                    <h3 className = "card-title">User Login</h3> 
+                    <form onSubmit = {onSubmitHandler} className = "card-body">
+                        {
+                            errs?
+                            <span className = "text-danger">{errs}</span>:
+                            null
+                        }
+                        <div className = "form-group row justify-content-center">
+                            <label htmlFor = "email" className = "col-form-label col-md-2">Email</label>
+                            <div className = "col-md-8">
+                                <input 
+                                    type = "text"
+                                    name = "email"
+                                    onChange = {(e) => setEmail(e.target.value)}
+                                    placeholder = "Please enter your email."
+                                    className = "form-control"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className = "form-group row justify-content-center mb-2">
-                        <label htmlFor = "password" className = "col-form-label col-md-4">Password</label>
-                        <div className = "col-md-8">
-                            <input 
-                                type = "password"
-                                name = "password"
-                                onChange = {(e) => setPassword(e.target.value)}
-                                placeholder = "Please enter your password."
-                                className = "form-control"
-                            />  
+                        {
+                            fieldEmpty.emailMsg?
+                            <div className = "form-group row">
+                                <div className = "col form-text text-danger">
+                                    {fieldEmpty.emailMsg}   
+                                </div>
+                            </div>:
+                            null
+                        }
+                        <div className = "form-group row justify-content-center mt-2">
+                            <label htmlFor = "password" className = "col-form-label col-md-2">Password</label>
+                            <div className = "col-md-8">
+                                <input 
+                                    type = "password"
+                                    name = "password"
+                                    onChange = {(e) => setPassword(e.target.value)}
+                                    placeholder = "Please enter your password."
+                                    className = "form-control"
+                                />  
+                            </div>
                         </div>
-                    </div>
-                    <div className = "form-group row justify-content-center">
-                        <div className = "col-md-3 col-lg-1">
-                            <input 
-                                type = "submit"
-                                value = "Login"
-                                className = "btn btn-primary"
-                            />
+                        {
+                            fieldEmpty.passwordMsg?
+                            <div className = "form-group row">
+                                <div className = "col form-text text-danger">
+                                    {fieldEmpty.passwordMsg}   
+                                </div>
+                            </div>:
+                            null
+                        }
+                        <div className = "form-group row justify-content-center mt-2">
+                            <div className = "col">
+                                <input 
+                                    type = "submit"
+                                    value = "Login"
+                                    className = "btn btn-primary"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    {
-                        errs?
-                        <span className = "text-danger">{errs}</span>:
-                        null
-                    }
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     )
