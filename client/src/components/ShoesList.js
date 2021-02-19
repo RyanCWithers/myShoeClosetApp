89884c51a0d7ba19044956d07f57800de50760d4
RyanCWithers <rcwithers17@gmail.com>
2021-02-18 20:20:33 -0500

    Styled shoelist components

--- client/src/components/ShoesList.js
+++ client/src/components/ShoesList.js
@@ -6,23 +6,26 @@ const ShoesList = ({shoes}) =>{
  
     return(
         <div className = "container-sm">
-            <div className = "row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+            <div className = "row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
                 {shoes.map((item, index) =>(
                     <div className = "col">
-                        <div key = {index} className = "card list-group-item-action" onClick = {() => navigate("/api/myShoeCloset/user/" + item._id)}>
-                        {
-                            item.shoeImgLink?
-                            <img src = {item.shoeImgLink} alt = "shoe" className = "img-fluid"/>:
-                            <img src = {stockShoeImg} alt = "blank shoe" className = "img-fluid"/>
-                        }
-                            <div>
-                                <p>{item.shoeName}</p>
-                                <p>{item.shoeCompany}</p>
-                                <p>{item.shoeSize}</p>
+                        <div class = "shoeCard">
+                            <div key = {index} className = "h-100" onClick = {() => navigate("/api/myShoeCloset/user/" + item._id)}>
+                                <div class = "imgContain">
+                                    {
+                                        item.shoeImgLink?
+                                        <img src = {item.shoeImgLink} alt = "shoe" class = "shoeImg"/>:
+                                        <img src = {stockShoeImg} alt = "blank shoe" class = "shoeImg"/>
+                                    }
+                                </div>
+                                <div class = "shoeCardInfo">
+                                    <p>{item.shoeName}</p>
+                                    <p>{item.shoeCompany}</p>
+                                    <p>{item.shoeSize}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    
                 ))}
             </div>
         </div>
